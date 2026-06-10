76d683faa8c539cd5f450718b145c33242c126de
Lindsay Scott <amberrsprivate@Lindsays-MacBook-Air.local>
2026-06-10 19:41:38 -0400

    Allow unsafe werkzeug and remove gunicorn

--- app.py
+++ app.py
@@ -977,4 +977,4 @@ def serve_react(path):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, debug=False, host='0.0.0.0', port=port)
+    socketio.run(app, debug=False, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
